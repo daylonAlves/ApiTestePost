ebcc2b96e1839144e7cb6fca5fb538ac20a3e9e6
daylonAlves <daylonsalesalves@gmail.com>
2024-07-29 08:54:00 -0300

    feat:ApiTestePost

---
+++ ApiTestePost/Data/ApiTestePostContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ApiTestePost.Models;
+
+namespace ApiTestePost.Data
+{
+    public class ApiTestePostContext : DbContext
+    {
+        public ApiTestePostContext (DbContextOptions<ApiTestePostContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ApiTestePost.Models.Produto> Produto { get; set; }
+    }
+}
